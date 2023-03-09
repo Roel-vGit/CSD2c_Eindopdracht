d5b468a8ed56820b5b58223bca1b7ac94b02b763
Jensnln <jens.loeven@student.hku.nl>
2023-03-09 15:15:05 +0100

    new source and header file

---
+++ waveshaper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jens on 09/03/2023.
+//
+
+#include "waveshaper.h"
---
+++ waveshaper.h
@@ -0,0 +1,8 @@
+//
+// Created by Jens on 09/03/2023.
+//
+
+#ifndef CSD_WAVESHAPER_H
+#define CSD_WAVESHAPER_H
+
+#endif //CSD_WAVESHAPER_H
