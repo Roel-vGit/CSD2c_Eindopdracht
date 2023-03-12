3e8b53df4f9dfe8284d8442561f5041130a237e0
Jensnln <jens.loeven@student.hku.nl>
2023-03-12 14:02:43 +0100

    Temp stage

---
+++ .jens/test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Jens on 12/03/2023.
+//
+
+#include "../libs/Util.h"
+
+int main() {
+	std::string i;
+	std::cout << "Hello, World!" << std::endl;
+	std::cin >> i;
+	std::cout << i << std::endl;
+	return 0;
+}
