cfde34c3bf280cde20ed5ac718f4bdcba447d58c
Roel Vugts <roel.vugts@yahoo.com>
2023-03-15 13:55:47 +0100

    Split decorrelator header

---
+++ include/filter.h
@@ -0,0 +1,64 @@
+#include "effect.h"
+#include "../libs/circularBuffer.h"
+#include "../libs/util.h"
+#include <cmath>
+#include <iostream>
+
+#pragma once
+
+class Filter : public Effect {
+    public:
+
+        Filter();
+        ~Filter();
+
+        //initialize delay buffers
+        virtual void prepareToPlay(int samplerate) override;
+
+        /*Sets the cutoff frequency of the lowpass and highpass filters
+        cutoff: cutoff frequency in Hz */
+        void setCutoff(float cutoff);
+
+    protected:
+        float cutoff;
+        float output1 { 0.0f }; //1 sample delay
+        float output2; // 2 sample delay
+};
+
+class Lowpass : public Filter {
+    public:
+        void calculate(const float& input, float& output) override;
+
+};
+
+class Highpass : public Filter {
+    public:
+        void calculate(const float& input, float& output) override;
+
+};
+
+class Allpass : public Filter {
+    public:
+
+        void prepareToPlay(int samplerate) override;
+
+        void calculate(const float& input, float& output) override;
+
+        /*Sets the allpass filter coefficients and delay
+        gain: filter coefficient (value between 0.0f and 1.0f)
+        delay: delay of the allpass filter*/
+        void setAllpass(float gain, float delay);
+
+        //returns the delay of the allpass filter
+        float getAllpassDelay() const;
+
+        //returns the feedback (coefficient) of the allpass
+        float getAllpassFeedback() const;
+        
+        CircBuffer<float, uint> circBuf = { CircBuffer<float, uint>(48000) }; //delay buffer for allpass input X[n-d]
+        CircBuffer<float, uint> filterBuf = { CircBuffer<float, uint>(48000) }; //delay buffer for allpass output (Y[n-d])
+        float allpassFeedback { 0.2f };
+        float allpassDelay { 5 };
+        float gainFactor { 1.0f };
+        float delayFactor { 1.0 };
+};
---
+++ src/filter.cpp
@@ -0,0 +1,68 @@
+#include "../include/filter.h"
+#include "circularBuffer.cpp"
+
+Filter::Filter() {};
+
+Filter::~Filter() {};
+
+//initialize delay buffer size to 1 second
+void Filter::prepareToPlay(int samplerate)
+{
+    this->sampleRate = samplerate;
+}
+
+void Filter::setCutoff(float cutoff)
+{
+    if (cutoff > sampleRate/2)
+        cutoff = sampleRate/2;
+    else if (cutoff < 0)
+        cutoff = 0.0;
+
+    this->cutoff = sin(cutoff*(M_PI/sampleRate)); //calculate cutoff -> https://docs.cycling74.com/max7/refpages/onepole~
+}
+
+void Lowpass::calculate(const float& input, float& output)
+{
+    output1 += cutoff * (input - output1); //y(n) = y(n-1) + a_{0}[ x(n) - y(n-1) ] https://docs.cycling74.com/max7/refpages/onepole~
+    output2 += cutoff * (output1 - output2); //2nd order lowpass
+    output = output2;
+}
+
+void Highpass::calculate(const float& input, float& output) 
+{
+    output1 += cutoff * (input - output1);
+    output2 += cutoff * (output1 - output2); 
+    output = input - output2; //subtract original signal from lowpass signal to get highpass signal
+}
+
+void Allpass::prepareToPlay(int samplerate)
+{
+    this->sampleRate = samplerate;
+    circBuf.setSize(samplerate);
+    filterBuf.setSize(samplerate);
+}
+
+void Allpass::calculate(const float& input, float& output)
+{   
+    circBuf.writeSample(input);
+    output = (allpassFeedback* gainFactor * -1 * input) + circBuf.readSample(allpassDelay * delayFactor) + (allpassFeedback * filterBuf.readSample(allpassDelay));// y[n] = (-g * x[n]) + x[n - d] + (g * y[n - d])
+    filterBuf.writeSample(output1);
+    circBuf.incrementWrite();
+    filterBuf.incrementWrite();
+}
+
+void Allpass::setAllpass(float gain, float delay)
+{
+    this->allpassFeedback = gain;
+    this->allpassDelay = delay;
+}
+
+float Allpass::getAllpassDelay() const
+{
+    return allpassDelay;
+}
+
+float Allpass::getAllpassFeedback() const
+{
+    return allpassFeedback;
+}
