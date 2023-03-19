9d512a17b425bdb3151e073193a644f09cf53750
Roel Vugts <roel.vugts@yahoo.com>
2023-03-19 17:08:45 +0100

    Mutex for OSC thread

--- Bela/render.cpp
+++ Bela/render.cpp
@@ -22,6 +22,9 @@ The data is sent via OSC to the laptop connected to the Bela running a C++ progr
 #include <libraries/OscSender/OscSender.h>
 #include <libraries/Trill/Trill.h>
 #include <iostream>
+#include <pthread.h>
+
+pthread_mutex_t oscMutex;
 
 OscSender oscSender;
 Trill touchPad1;
@@ -77,18 +80,24 @@ void sendOSC(void*)
 		usleep(auxTaskSleepTime); 
 		
 		// Joystick value at center posititon is { 0.5, 0.5 }, scale this so the center becomes { 0.0, 0.0 }
+		__wrap_pthread_mutex_lock(&oscMutex);
 		xPos = xPos * 2.0f - 0.984f;
 		yPos = yPos * 2.0f - 1.0f;
-		if (xPos < 0.01f && xPos > 0.001f)
-			xPos = 0.0f;
-		if (yPos < 0.03f && yPos > 0.015f)
-			yPos = 0.0f;
+
 		xPos2 = xPos2 * 2.0f - 0.984f;
 		yPos2 = yPos2 * 2.0f - 1.0f;
-		
+
+		// xPos = 0.0f;
+		// yPos = 0.0f;
+		// xPos2 = 0.0f;
+		// yPos2 = 0.0f;
+
+		// std::cout << "xPos: " << xPos2 << " " << "yPos: " << yPos2 << std::endl;
 		// Send the joystick values via OSC
 		oscSender.newMessage("/joystick1").add(xPos).add(yPos).send();
 		oscSender.newMessage("/joystick2").add(xPos2).add(yPos2).send();
+		__wrap_pthread_mutex_unlock(&oscMutex);
+
 	}
 
 
@@ -116,6 +125,7 @@ bool setup(BelaContext *context, void *userData)
 	
 	touchPad1.printDetails();
 	touchPad2.printDetails();
+	touchPad2.setNoiseThreshold(0.02);
 	
 	oscSender.setup(remotePort, remoteIP);
 	oscSender.newMessage("/osc-connection").add("hello").send();
@@ -125,6 +135,12 @@ bool setup(BelaContext *context, void *userData)
 
 	// Calculate amount of audio frames per analog frame
 	gAudioFramesPerAnalogFrame = context->audioFrames / context->analogFrames;
+	
+	if (__wrap_pthread_mutex_init(&oscMutex, NULL))
+		printf("unable to initialize mutex");
+	
+	
+	
 	
 	return true;
 }
@@ -138,15 +154,17 @@ void render(BelaContext *context, void *userData)
 			// Depending on the sampling rate of the analog inputs, this will
 			// happen every audio frame (if it is 44100)
 			// or every two audio frames (if it is 22050)
-			yPos = map(analogRead(context, n/gAudioFramesPerAnalogFrame, joystickX), 0.0f, 0.82f, 0.0f, 1.0f);
-			xPos = map(analogRead(context, n/gAudioFramesPerAnalogFrame, joystickY), 0.0f, 0.82f, 0.0f, 1.0f);
-			yPos2 = map(analogRead(context, n/gAudioFramesPerAnalogFrame, joystick2X), 0.0f, 0.82f, 0.0f, 1.0f);
-			xPos2 = map(analogRead(context, n/gAudioFramesPerAnalogFrame, joystick2Y), 0.0f, 0.82f, 0.0f, 1.0f);
+			__wrap_pthread_mutex_lock(&oscMutex);
+			yPos = map(analogRead(context, n/gAudioFramesPerAnalogFrame, joystickX), 0.0f, 0.824f, 0.0f, 1.0f);
+			xPos = map(analogRead(context, n/gAudioFramesPerAnalogFrame, joystickY), 0.0f, 0.824f, 0.0f, 1.0f);
+			yPos2 = map(analogRead(context, n/gAudioFramesPerAnalogFrame, joystick2X), 0.0f, 0.824f, 0.0f, 1.0f);
+			xPos2 = map(analogRead(context, n/gAudioFramesPerAnalogFrame, joystick2Y), 0.0f, 0.824f, 0.0f, 1.0f);
+			__wrap_pthread_mutex_unlock(&oscMutex);
 		}
 	}
 }	
 
 void cleanup(BelaContext *context, void *userData)
 {
-
+	__wrap_pthread_mutex_destroy(&oscMutex);
 }
